0278c1070041a360b120b5842d2be62f56608282
goenjoy <goenjoy@khadas.com>
2019-07-08 10:59:46 +0800

    enble force_land mode

--- device.mk
+++ device.mk
@@ -132,4 +132,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
                 ro.sf.lcd_density=280 \
                 ro.build.shutdown_timeout=6 \
                 persist.enable_task_snapshots=false \
+				persist.sys.app.rotation=force_land \
                 persist.sys.hdmi.cec_enable=true
