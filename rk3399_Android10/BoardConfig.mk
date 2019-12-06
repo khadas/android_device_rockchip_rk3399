40da0c0d706d57f0d0b696d5cb28226641e44623
Wu Liangqing <wlq@rock-chips.com>
2019-12-06 15:51:51 +0800

    rk3399_Android10: set userdata with ext4 filesystem
    Change-Id: Ie617f2b987a0314f923dbf30227f73d441b397d1

--- rk3399_Android10/BoardConfig.mk
+++ rk3399_Android10/BoardConfig.mk
@@ -4,7 +4,6 @@ BOARD_SENSOR_ST := true
 BOARD_SENSOR_MPU_PAD := false
 BUILD_WITH_GOOGLE_GMS_EXPRESS := false
 CAMERA_SUPPORT_AUTOFOCUS:= false
-
 ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
 TARGET_RECOVERY_FSTAB := device/rockchip/rk3399/rk3399_Android10/fstab.rk30board_AB
 endif
