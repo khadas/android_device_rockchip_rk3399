c848a278e8102118c802ec517f7412074f4bd438
Wang Jie <dave.wang@rock-chips.com>
2019-12-10 10:07:22 +0800

    rk3399_Android10: enable akm8963.
    Change-Id: Ib507d748405e1b29a18c3a22e8cecc0a3ea7eb9b
    Signed-off-by: Wang Jie <dave.wang@rock-chips.com>

--- rk3399_Android10/BoardConfig.mk
+++ rk3399_Android10/BoardConfig.mk
@@ -1,7 +1,9 @@
 include device/rockchip/rk3399/BoardConfig.mk
 
 BOARD_SENSOR_ST := true
+BOARD_SENSOR_COMPASS_AK8963-64 = true
 BOARD_SENSOR_MPU_PAD := false
+BOARD_COMPASS_SENSOR_SUPPORT := true
 BUILD_WITH_GOOGLE_GMS_EXPRESS := false
 CAMERA_SUPPORT_AUTOFOCUS:= false
 ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
