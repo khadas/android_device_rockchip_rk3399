54b362497944622117bb345d3fbf7b8ed758fa30
Dayao Ji <jdy@rock-chips.com>
2019-12-11 16:39:05 +0800

    rk3399_Android10: add A/B support, disabled by default
    Change-Id: I080bed75955939bd05e49959af0e45fff7d082a5
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>

--- rk3399_Android10/BoardConfig.mk
+++ rk3399_Android10/BoardConfig.mk
@@ -7,6 +7,11 @@ BOARD_COMPASS_SENSOR_SUPPORT := true
 BOARD_GYROSCOPE_SENSOR_SUPPORT := true
 BUILD_WITH_GOOGLE_GMS_EXPRESS := false
 CAMERA_SUPPORT_AUTOFOCUS:= false
+
+# AB image definition
+BOARD_USES_AB_IMAGE := false
+
 ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
-TARGET_RECOVERY_FSTAB := device/rockchip/rk3399/rk3399_Android10/fstab.rk30board_AB
+    include device/rockchip/common/BoardConfig_AB.mk
+    TARGET_RECOVERY_FSTAB := device/rockchip/rk3399/rk3399_Android10/recovery.fstab_AB
 endif
--- rk3399_Android10/rk3399_Android10.mk
+++ rk3399_Android10/rk3399_Android10.mk
@@ -21,8 +21,8 @@ PRODUCT_DTBO_TEMPLATE := $(LOCAL_PATH)/dt-overlay.in
 PRODUCT_BOOT_DEVICE := fe330000.sdhci
 include device/rockchip/common/build/rockchip/DynamicPartitions.mk
 include device/rockchip/common/BoardConfig.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 include device/rockchip/rk3399/rk3399_Android10/BoardConfig.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/rockchip/rk3399/device.mk)
 $(call inherit-product, device/rockchip/common/device.mk)
