90e812e755a5c4518c98f1a17baa0633cef54af0
goenjoy <goenjoy@khadas.com>
2020-05-12 20:38:03 +0800

    CEC: enble hdmi.cec [2/2]
    Change-Id: I5db760bcc7b04b04ecdf5cf7da777e1d300454d0

--- device.mk
+++ device.mk
@@ -175,4 +175,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
                 ro.kernel.android.checkjni=0 \
                 ro.sf.lcd_density=280 \
                 ro.build.shutdown_timeout=6 \
+		persist.sys.hdmi.cec_enable=true \
                 persist.enable_task_snapshots=false
